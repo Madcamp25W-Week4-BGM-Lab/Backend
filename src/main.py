c849224e32f05c9e1f605114b2af9d765129d77f
Paul Bin <paul.bin0117@gmail.com>
2026-02-02 22:03:20 +0900

    cleaned up main including readme

--- src/main.py
+++ src/main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI
 from src.commit.router import router as commit_router
+from src.readme.router import router as readme_router
 from src.infrastructure.router import router as infrastructure_router
+
 
 
 app = FastAPI(title="SubText Backend")
 
 # Mount Domain Routers (Prefix /api/v1)
 app.include_router(commit_router, prefix="/api/v1", tags=["Commit"])
-# TODO: add Readme router
+app.include_router(readme_router, prefix="/api/v1", tags=["ReadMe"])
 
 # Mount Infrastructure Router
 app.include_router(infrastructure_router, tags=["Infrastructure"])
 
-from src.readme.router import router as readme_router
-
-app = FastAPI()
 
 @app.get("/", tags=["health"])
 def root():
@@ -25,5 +24,3 @@ def root():
         "readme_api": "/api/readme/generate",
     }
 
-# README generation API
-app.include_router(readme_router)
